6a6317dfe055c5edd9d670cf9dfdd353c922356a
Summer <csy13808796047@gmail.com>
2021-08-31 15:13:18 +0800

    重构 GetRouteVariable

--- pkg/route/router.go
+++ pkg/route/router.go
@@ -1,6 +1,9 @@
 package route
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 // Router 路由对象
 var Router *mux.Router
@@ -17,4 +20,10 @@ func RouteName2URL(routeName string, pairs ...string) string {
 		return ""
 	}
 	return url.String()
+}
+
+// GetRouteVariable 获取URI路由参数
+func GetRouteVariable(parameterName string, r *http.Request) string {
+	vars := mux.Vars(r)
+	return vars[parameterName]
 }
