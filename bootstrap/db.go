d645af1e8a40a6ca9f01f25524c729194f1f4802
Summer <csy13808796047@gmail.com>
2021-08-31 17:39:44 +0800

    创建用户

--- bootstrap/db.go
+++ bootstrap/db.go
@@ -1,7 +1,10 @@
 package bootstrap
 
 import (
+	"github.com/13808796047/go-blog/app/models/article"
+	"github.com/13808796047/go-blog/app/models/user"
 	"github.com/13808796047/go-blog/pkg/model"
+	"gorm.io/gorm"
 	"time"
 )
 
@@ -17,4 +20,14 @@ func SetupDB() {
 	sqlDB.SetMaxIdleConns(25)
 	// 设置每个链接的过期时间
 	sqlDB.SetConnMaxLifetime(5 * time.Minute)
+	// 创建和维护数据表结构
+	migration(db)
+}
+func migration(db *gorm.DB) {
+
+	// 自动迁移
+	db.AutoMigrate(
+		&user.User{},
+		&article.Article{},
+	)
 }
