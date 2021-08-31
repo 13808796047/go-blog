82b0e3b8b13311665d1d0c4a187c23d19b7a8660
Summer <csy13808796047@gmail.com>
2021-08-31 13:34:13 +0800

    使用模板文件

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"net/url"
 	"strings"
 	"unicode/utf8"
 
@@ -10,6 +12,13 @@ import (
 )
 
 var router = mux.NewRouter()
+
+// ArticlesFormData 创建博文表单数据
+type ArticlesFormData struct {
+	Title, Body string
+	URL         *url.URL
+	Errors      map[string]string
+}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
@@ -53,16 +62,29 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	} else if utf8.RuneCountInString(body) < 10 {
 		errors["body"] = "内容长度需大于或等于 10 个字节"
 	}
-	// 检查是否有错误
-	if len(errors) == 0 {
-		fmt.Fprint(w, "验证通过!<br>")
-		fmt.Fprintf(w, "title 的值为: %v <br>", title)
-		fmt.Fprintf(w, "title 的长度为: %v <br>", len(title))
-		fmt.Fprintf(w, "body 的值为: %v <br>", body)
-		fmt.Fprintf(w, "body 的长度为: %v <br>", len(body))
-	} else {
-		fmt.Fprintf(w, "有错误发生，errors 的值为: %v <br>", errors)
+	//// 检查是否有错误
+	//if len(errors) == 0 {
+	//	fmt.Fprint(w, "验证通过!<br>")
+	//	fmt.Fprintf(w, "title 的值为: %v <br>", title)
+	//	fmt.Fprintf(w, "title 的长度为: %v <br>", len(title))
+	//	fmt.Fprintf(w, "body 的值为: %v <br>", body)
+	//	fmt.Fprintf(w, "body 的长度为: %v <br>", len(body))
+	//} else {
+	//	fmt.Fprintf(w, "有错误发生，errors 的值为: %v <br>", errors)
+	//}
+	storeURL, _ := router.Get("articles.store").URL()
+	data := ArticlesFormData{
+		Title:  title,
+		Body:   body,
+		URL:    storeURL,
+		Errors: errors,
 	}
+	tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	tmpl.Execute(w, data)
 }
 
 func forceHTMLMiddleware(next http.Handler) http.Handler {
@@ -87,23 +109,19 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 	})
 }
 func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
-	html := `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>创建文章 —— 我的技术博客</title>
-</head>
-<body>
-    <form action="%s" method="post">
-        <p><input type="text" name="title"></p>
-        <p><textarea name="body" cols="30" rows="10"></textarea></p>
-        <p><button type="submit">提交</button></p>
-    </form>
-</body>
-</html>
-`
 	storeURL, _ := router.Get("articles.store").URL()
-	fmt.Fprintf(w, html, storeURL)
+	data := ArticlesFormData{
+		Title:  "",
+		Body:   "",
+		URL:    storeURL,
+		Errors: nil,
+	}
+	tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	tmpl.Execute(w, data)
 }
 func main() {
 
