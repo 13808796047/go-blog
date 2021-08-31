68e86c7312eebb8f9b3c16ba77bea2023386ea37
Summer <csy13808796047@gmail.com>
2021-08-31 16:28:57 +0800

    重构中间件

--- main.go
+++ main.go
@@ -1,59 +1,15 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
+	"github.com/13808796047/go-blog/app/http/middlewares"
 	"github.com/13808796047/go-blog/bootstrap"
-	"github.com/13808796047/go-blog/pkg/database"
-	"github.com/13808796047/go-blog/pkg/logger"
-	"github.com/13808796047/go-blog/pkg/route"
-	"github.com/13808796047/go-blog/pkg/types"
-	"github.com/go-sql-driver/mysql"
-	"html/template"
 	"net/http"
-	"net/url"
-	"strconv"
-	"strings"
-	"time"
-	"unicode/utf8"
-
-	"github.com/gorilla/mux"
 )
 
-func forceHTMLMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1. 设置标头
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		// 2. 继续处理请求
-		next.ServeHTTP(w, r)
-	})
-}
-
-// 去除路由"/"
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1. 除首页以外，移除所有请求路径后面的斜杆
-		if r.URL.Path != "/" {
-
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-		// 将请求传递下去
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
+
 	bootstrap.SetupDB()
-	router = bootstrap.SetupRoute()
+	router := bootstrap.SetupRoute()
 
-	// 中间件：强制内容类型为 HTML
-	router.Use(forceHTMLMiddleware)
-
-	// 通过命名路由获取 URL 示例
-	homeURL, _ := router.Get("home").URL()
-	fmt.Println("homeURL: ", homeURL)
-	articleURL, _ := router.Get("articles.show").URL("id", "1")
-	fmt.Println("articleURL: ", articleURL)
-
-	http.ListenAndServe(":3000", removeTrailingSlash(router))
+	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
 }
