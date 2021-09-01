60318630d9a7ea0b0700fd1df33670fb3bd974c7
Summer <csy13808796047@gmail.com>
2021-09-01 09:58:20 +0800

    会话和认证管理

--- pkg/session/session.go
+++ pkg/session/session.go
@@ -8,28 +8,30 @@ import (
 
 // Store gorilla session的存储库
 var Store = sessions.NewCookieStore([]byte("33446a9dcf9ea060a0a6532b166da32f304af0de"))
+
 // Session 当前会话
 var Session *sessions.Session
 
 // Request用于获取会话
 
 var Request *http.Request
+
 // Response 用于写入会话
 
 var Response http.ResponseWriter
 
-func StartSession(w http.ResponseWriter,r *http.Request)  {
+func StartSession(w http.ResponseWriter, r *http.Request) {
 	var err error
 	// Store.Get()的第二个参数是Cookie的名称
 	// gorilla/session支持多会话,本项目我们只使用单一会话即可
-	Session,err = Store.Get(r,"goblog-session")
+	Session, err = Store.Get(r, "goblog-session")
 	logger.LogError(err)
 	Request = r
 	Response = w
 }
 
 // Put写入键值对应的会话数据
-func Put(key string,value interface{})  {
+func Put(key string, value interface{}) {
 	Session.Values[key] = value
 	Save()
 }
@@ -38,21 +40,24 @@ func Put(key string,value interface{})  {
 func Get(key string) interface{} {
 	return Session.Values[key]
 }
+
 // forget 删除某个会话项
-func forget(key string)  {
-	delete(Session.Values,key)
+func Forget(key string) {
+	delete(Session.Values, key)
 	Save()
 }
+
 // Flush 删除当前会话
-func Flush()  {
+func Flush() {
 	Session.Options.MaxAge = -1
 	Save()
 }
+
 // Save保持会话
-func Save(){
+func Save() {
 	// 非HTTPS的链接无法使用Secure和HttpOnly,浏览器会报错
 	// Session.Options.Secure = true
 	// Session.Options.HttpOnly = true
-	err := Session.Save(Request,Response)
+	err := Session.Save(Request, Response)
 	logger.LogError(err)
 }
